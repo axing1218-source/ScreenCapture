7b62008a940c101ea84e35dde3b8ecf3f173ec88
axing1218-source <axing1218@gmail.com>
2026-08-25 14:02:18 +0800

    Add Gemini controls to common settings

--- Src/Win/WinSettingCommon.h
+++ Src/Win/WinSettingCommon.h
@@ -9,12 +9,16 @@ public:
 private:
 	void initAutoStartCtrls();
 	void initCaptureBorderCtrls();
+	void initGeminiCtrls();
 	void initLangCtrls();
 	void setAutoStartBtn(Ling::Button* btn);
 	void showSelectBox(Ling::Button* btn);
 private:
 	Ling::Button* selectBtn{ nullptr };
 	Ling::Label* borderWidthLabel{ nullptr };
+	Ling::TextBox* geminiApiKeyBox{ nullptr };
+	Ling::TextBox* geminiModelBox{ nullptr };
+	Ling::Label* geminiStatus{ nullptr };
 	Ling::ScrollerBox* selectBox{ nullptr };
 	winrt::event_token onMouseDownToken;
 };
