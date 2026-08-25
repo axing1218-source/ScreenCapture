b75201a44e6f27c46e47e07c27582a2e4f6007e5
axing1218-source <axing1218@gmail.com>
2026-08-25 13:31:28 +0800

    Stabilize repeated OCR result window launches

--- Src/WeShotOcr.h
+++ Src/WeShotOcr.h
@@ -1,3 +1,23 @@
 #pragma once
 #include "WeShotOcrV2.h"
-namespace WeShotOcr = WeShotOcrV2;
+
+namespace WeShotOcr
+{
+    inline bool containsPoint(POINT pos)
+    {
+        return WeShotOcrV2::containsPoint(pos);
+    }
+
+    inline bool hasWindow()
+    {
+        return WeShotOcrV2::hasWindow();
+    }
+
+    inline void show(WinCap* win)
+    {
+        // Close an older result window before the new request receives its request id.
+        // Otherwise the old window's onDestroy invalidates the freshly-created OCR request.
+        if (WeShotOcrV2::activeWindow) WeShotOcrV2::activeWindow->close();
+        WeShotOcrV2::show(win);
+    }
+}
