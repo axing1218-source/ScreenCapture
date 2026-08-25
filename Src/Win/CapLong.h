788e43fd07f1d2568c15e0c6426d649768699c92
axing1218-source <axing1218@gmail.com>
2026-08-25 11:53:45 +0800

    Make long screenshot manual-first with optional auto scroll

--- Src/Win/CapLong.h
+++ Src/Win/CapLong.h
@@ -3,57 +3,46 @@
 
 class WinCap;
 class ToolLong;
-// 滚动截图。选区已经由 WinCap 定好了，所以它不是窗口，画布、定时器、光标全借宿主的。
+
 class CapLong
 {
 public:
 	CapLong(WinCap* win);
 	~CapLong();
-	// 宿主窗口要销毁了：停掉定时器、收掉工具条
 	void dispose();
-	// 以下几个都由 WinCap 在对应的事件里转进来
 	void onMove(POINT pos);
 	void onUp(POINT pos);
 	void onTimerCB(UINT timerId);
 	void setCursor();
 	void paint(ID2D1DeviceContext* ctx);
-	// 三个都是 ToolLong 的按钮动作，做完由 ToolLong 关掉宿主窗口。
-	// saveToFile 返回是否真的存下来了：用户在另存为对话框里按了取消时不该收工
 	void copyToClipboard();
 	bool saveToFile();
 	void pin();
-	// Ctrl+S / Ctrl+C 用：还没点"开始"的时候一张图都没有，此时快捷键不该生效
 	bool hasImage() const { return !imgData.empty(); }
-	// ToolLong 的摆放规则：摆在选区右侧（右边放不下就改到左侧），底边与选区底边齐。
-	// 建窗口时走一遍，工具条的 DPI 变了之后由它回头再走一遍
 	void layoutTool();
+	// Manual capture is the default. This switches the already-running session to automatic scrolling.
+	void startAutoScroll();
 private:
 	void firstStep();
 	void makeImgPreview();
 	void capStep();
+	void scheduleNextCapture(int delayMs = 120);
 	void makeTool();
 	void paintImgPreview(ID2D1DeviceContext* ctx);
 	void stopCap();
 	void makeStopText();
 private:
 	WinCap* win;
-	bool isShowStartBtn{ false }, isScrolling{ false }, isFinish{ false };
+	bool isCapturing{ false }, isFinish{ false }, autoScroll{ false };
 	bool firstCheck{ true };
 	int dismissTime{ 0 };
-	// 抓到"帧在变但匹配不出滚动量"的帧时连续等待的次数，防止一直卡住
 	int settleRecheckCount{ 0 };
 	int changeStartY{ -1 };
 	D2D1_RECT_F stopTextRect{};
-	// 两处文字的绘制起点。IDWriteTextLayout 默认左上对齐，DrawTextLayout 给的又是
-	// layout 框的左上角，所以得先测出文本实际宽高，才能算出居中要的那个起点
 	D2D1_POINT_2F stopTextPos{};
-	D2D1_SIZE_F startTextSize{};
 	Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> textBrush;
 	Microsoft::WRL::ComPtr<ID2D1SolidColorBrush> bgBrush;
-	Microsoft::WRL::ComPtr<IDWriteTextLayout> layoutTextStart;
 	Microsoft::WRL::ComPtr<IDWriteTextLayout> layoutTextEnd;
-	float startCircleR{ 30.f };
-	POINT circleCenter{};
 	HWND targetHwnd{ nullptr };
 	std::unique_ptr<ToolLong> tool;
 	Microsoft::WRL::ComPtr<ID2D1Bitmap1> imgPreview;
