86af9b98c17ef916077e3342e1af618d76ab8310
axing1218-source <axing1218@gmail.com>
2026-08-25 14:01:09 +0800

    Link WinHTTP and Crypt32 for Gemini integration

--- Src/pch.h
+++ Src/pch.h
@@ -2,4 +2,9 @@
 #include <include/Ling.h>
 #include <winrt/Windows.Data.Json.h>
 #include <winrt/Windows.Foundation.Collections.h>
+#include <format>
+#include <cmath>
+
+#pragma comment(lib, "winhttp.lib")
+#pragma comment(lib, "crypt32.lib")
 
