6376bf6a338077cdfb6b5ced5dfa16e6689808b6
axing1218-source <axing1218@gmail.com>
2026-09-03 07:05:31 +0800

    Add uTools-style clipboard UI wrapper

---
+++ Src/ClipboardHistoryV099Utools.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "ClipboardHistoryV099Utools_part1.inc"
+#include "ClipboardHistoryV099Utools_part2.inc"
+#include "ClipboardHistoryV099Utools_part3.inc"
+#include "ClipboardHistoryV099Utools_part4.inc"
+#include "ClipboardHistoryV099Utools_part5.inc"
+#include "ClipboardHistoryV099Utools_part6.inc"
+#include "ClipboardHistoryV099Utools_part7.inc"
+#include "ClipboardHistoryV099Utools_part8.inc"
+#include "ClipboardHistoryV099Utools_part9.inc"
