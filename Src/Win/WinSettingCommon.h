a62856fa645811598de5cd1873a5737ee06144a7
axing1218-source <axing1218@gmail.com>
2026-09-02 15:23:42 +0800

    Refactor common settings declarations

--- Src/Win/WinSettingCommon.h
+++ Src/Win/WinSettingCommon.h
@@ -1,5 +1,10 @@
 ﻿#pragma once
 #include <include/Ling.h>
+#include <functional>
+#include <string>
+#include <utility>
+#include <vector>
+
 class WinSettingCommon:public Ling::Node
 {
 public:
@@ -9,16 +14,25 @@ public:
 private:
 	void initAutoStartCtrls();
 	void initCaptureBorderCtrls();
-	void initGeminiCtrls();
+	void initAiCtrls();
 	void initLangCtrls();
 	void setAutoStartBtn(Ling::Button* btn);
+	void refreshAiControls();
+	void showAiProviderBox();
+	void showAiModelBox();
 	void showSelectBox(Ling::Button* btn);
+	void showChoiceBox(Ling::Button* btn,
+		const std::vector<std::pair<std::wstring, std::wstring>>& items,
+		std::function<void(const std::wstring&)> onChoose);
 private:
 	Ling::Button* selectBtn{ nullptr };
 	Ling::Label* borderWidthLabel{ nullptr };
-	Ling::TextBox* geminiApiKeyBox{ nullptr };
-	Ling::TextBox* geminiModelBox{ nullptr };
-	Ling::Label* geminiStatus{ nullptr };
+	Ling::Button* aiProviderBtn{ nullptr };
+	Ling::Label* aiKeyLabel{ nullptr };
+	Ling::TextBox* aiApiKeyBox{ nullptr };
+	Ling::Button* aiModelBtn{ nullptr };
+	Ling::Label* aiStatus{ nullptr };
+	std::vector<std::wstring> aiModels;
 	Ling::ScrollerBox* selectBox{ nullptr };
 	winrt::event_token onMouseDownToken;
 };
