be822cddf7c6b07ed87e7e2c65dfb5701b85f7e4
axing1218-source <axing1218@gmail.com>
2026-09-03 02:32:06 +0800

    Trigger v0.9.8 test package build

--- Src/Version.h
+++ Src/Version.h
@@ -1,6 +1,7 @@
 #pragma once
 
 // StarCap product version.
+// v0.9.8 test package.
 #define STARCAP_FILE_VERSION 0,9,8,0
 #define STARCAP_FILE_VERSION_STR "0.9.8.0"
 #define STARCAP_DISPLAY_VERSION L"v0.9.8"
