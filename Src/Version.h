aadfc1e68fd55e8e07ae1792eed970cb47f6b16a
axing1218-source <axing1218@gmail.com>
2026-09-04 06:29:48 +0800

    Bump StarCap version to v0.9.10

--- Src/Version.h
+++ Src/Version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 // StarCap product version.
-// v0.9.9 test package.
-#define STARCAP_FILE_VERSION 0,9,9,0
-#define STARCAP_FILE_VERSION_STR "0.9.9.0"
-#define STARCAP_DISPLAY_VERSION L"v0.9.9"
-#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.9.9"
+// v0.9.10 release.
+#define STARCAP_FILE_VERSION 0,9,10,0
+#define STARCAP_FILE_VERSION_STR "0.9.10.0"
+#define STARCAP_DISPLAY_VERSION L"v0.9.10"
+#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.9.10"
