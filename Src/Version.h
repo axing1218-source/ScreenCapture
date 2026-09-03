77b8c735375efcd78bb6049a55e58455580bd24e
axing1218-source <axing1218@gmail.com>
2026-09-04 07:40:56 +0800

    Bump StarCap to v0.10.0 Beta 2

--- Src/Version.h
+++ Src/Version.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // StarCap product version.
-// Public beta: v0.10.0 Beta 1.
-#define STARCAP_FILE_VERSION 0,10,0,1
-#define STARCAP_FILE_VERSION_STR "0.10.0.1"
+// Public beta: v0.10.0 Beta 2.
+#define STARCAP_FILE_VERSION 0,10,0,2
+#define STARCAP_FILE_VERSION_STR "0.10.0.2"
 #define STARCAP_DISPLAY_VERSION L"v0.10.0"
-#define STARCAP_PUBLIC_VERSION L"v0.10.0 Beta 1"
-#define STARCAP_RELEASE_TAG "v0.10.0-beta.1"
+#define STARCAP_PUBLIC_VERSION L"v0.10.0 Beta 2"
+#define STARCAP_RELEASE_TAG "v0.10.0-beta.2"
 #define STARCAP_RUNTIME_VERSION_STR "StarCap/0.10.0"
 #define STARCAP_PRERELEASE 1
