9cd429d494687a87ba7322babc40712fe17593e5
axing1218-source <axing1218@gmail.com>
2026-09-04 06:49:17 +0800

    Prepare StarCap v0.10.0 Beta 1 version metadata

--- Src/Version.h
+++ Src/Version.h
@@ -1,8 +1,11 @@
 #pragma once
 
 // StarCap product version.
-// v0.9.10 release.
-#define STARCAP_FILE_VERSION 0,9,10,0
-#define STARCAP_FILE_VERSION_STR "0.9.10.0"
-#define STARCAP_DISPLAY_VERSION L"v0.9.10"
-#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.9.10"
+// Public beta: v0.10.0 Beta 1.
+#define STARCAP_FILE_VERSION 0,10,0,1
+#define STARCAP_FILE_VERSION_STR "0.10.0.1"
+#define STARCAP_DISPLAY_VERSION L"v0.10.0"
+#define STARCAP_PUBLIC_VERSION L"v0.10.0 Beta 1"
+#define STARCAP_RELEASE_TAG "v0.10.0-beta.1"
+#define STARCAP_RUNTIME_VERSION_STR "StarCap/0.10.0"
+#define STARCAP_PRERELEASE 1
