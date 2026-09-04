f55ad41624b69458f4ef68c402b009013ac122de
axing1218-source <axing1218@gmail.com>
2026-09-04 14:30:21 +0800

    Expose default translation language in settings

--- Src/Win/WinSettingCommon.h
+++ Src/Win/WinSettingCommon.h
@@ -1,11 +1,22 @@
 ﻿#pragma once
 #include <include/Ling.h>
+#include "../StarCapTranslationLanguageSettings.h"
+
 class WinSettingCommon:public Ling::Node
 {
 public:
 	WinSettingCommon(Ling::WinBase* parent);
 	~WinSettingCommon();
 	void hideSelectBox();
+protected:
+	void layout() override
+	{
+		if (!translationLanguageInjected) {
+			translationLanguageInjected = true;
+			StarCapTranslationLanguageSettings::attach(this);
+		}
+		Ling::Node::layout();
+	}
 private:
 	void initAutoStartCtrls();
 	void initCaptureBorderCtrls();
@@ -21,4 +32,5 @@ private:
 	Ling::Label* geminiStatus{ nullptr };
 	Ling::ScrollerBox* selectBox{ nullptr };
 	winrt::event_token onMouseDownToken;
+	bool translationLanguageInjected{ false };
 };
