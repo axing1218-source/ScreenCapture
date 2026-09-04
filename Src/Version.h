dcc0412f2e2b1aa9f06166a56a9b6f242fd77f78
axing1218-source <axing1218@gmail.com>
2026-09-04 11:51:43 +0800

    Release StarCap v0.10.0 stable

--- Src/Version.h
+++ Src/Version.h
@@ -1,11 +1,11 @@
 #pragma once
 
 // StarCap product version.
-// Public beta: v0.10.0 Beta 2.
-#define STARCAP_FILE_VERSION 0,10,0,2
-#define STARCAP_FILE_VERSION_STR "0.10.0.2"
+// Stable release: v0.10.0.
+#define STARCAP_FILE_VERSION 0,10,0,0
+#define STARCAP_FILE_VERSION_STR "0.10.0.0"
 #define STARCAP_DISPLAY_VERSION L"v0.10.0"
-#define STARCAP_PUBLIC_VERSION L"v0.10.0 Beta 2"
-#define STARCAP_RELEASE_TAG "v0.10.0-beta.2"
+#define STARCAP_PUBLIC_VERSION L"v0.10.0"
+#define STARCAP_RELEASE_TAG "v0.10.0"
 #define STARCAP_RUNTIME_VERSION_STR "StarCap/0.10.0"
-#define STARCAP_PRERELEASE 1
+#define STARCAP_PRERELEASE 0
